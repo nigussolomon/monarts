46e102941eb71acbd866a434296eae91be2fd78b
nigussolomon <nigussolomon444@gmail.com>
2022-12-11 20:00:56 +0300

    added footer component

---
+++ src/Components/footer/footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./footer.css";
+
+export default function Footer(){
+    return(
+        <div className="footer">
+            <div className="head">
+                <a href="#" >monarts@gmail.com</a>
+            </div>
+            <div className="center">
+                <h2>MONA  ART  STUDIO</h2>
+            </div>
+            <div className="tail">
+                <a href="#">+251978616116</a>
+            </div>
+        </div>
+    )
+}
