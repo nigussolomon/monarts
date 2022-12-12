9166db97b812c3f925f03bff14b1f865c5255bf4
nigussolomon <nigussolomon444@gmail.com>
2022-12-12 21:38:23 +0300

    added route path to nav bar

--- src/Components/Navbar/navBar.js
+++ src/Components/Navbar/navBar.js
@@ -5,15 +5,15 @@ export default function NavBar(params){
     return(
         <div className="navbar">
             <div className="headNav">
-                <a href="" id={params.home}>HOME</a>
-                <a href="" id={params.paintings}>PAINTINGS</a>
+                <a href="/" id={params.home}>HOME</a>
+                <a href="/paintings" id={params.paintings}>PAINTINGS</a>
             </div>
             <div className="centerNav">
-                <h2>MONA  ART  STUDIO</h2>
+                <h2><a href="/">MONA  ART  STUDIO</a></h2>
             </div>
             <div className="tailNav">
-                <a href="" id={params.blog}>BLOG</a>
-                <a href="" id={params.request}>REQUEST</a>
+                <a href="/" id={params.blog}>BLOG</a>
+                <a href="/" id={params.request}>REQUEST</a>
             </div>
         </div>
     )
