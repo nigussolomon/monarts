78753f151d31f71c98994c959eed78e69c3b497e
nigussolomon <nigussolomon444@gmail.com>
2022-12-12 21:38:03 +0300

    added react routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import * as React from 'react';
+import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Layouts/Home";
+import Paintings from "./Layouts/Paintings";
 
 export default function App() {
   return (
     <div>
-      <Home></Home>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/paintings" element={<Paintings/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
